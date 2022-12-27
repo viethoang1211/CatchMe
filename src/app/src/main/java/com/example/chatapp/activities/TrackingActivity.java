ccb79a272af57d9e77efb858a38a61eb2ac1a1ee
Viet <87920454+vietAPCS@users.noreply.github.com>
2022-12-27 20:03:01 +0700

    map in tracking

--- src/app/src/main/java/com/example/chatapp/activities/TrackingActivity.java
+++ src/app/src/main/java/com/example/chatapp/activities/TrackingActivity.java
@@ -14,21 +14,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.chatapp.Class.TrackingHolder;
 import com.example.chatapp.R;
 import com.example.chatapp.Service.TrackingService;
-
-public class TrackingActivity extends AppCompatActivity {
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.chatapp.databinding.ActivityTrackingBinding;
+public class TrackingActivity extends FragmentActivity implements OnMapReadyCallback {
     Button btnStart, btnStop;
     TextView distance;
     TrackingHolder trackingHolder;
+    private GoogleMap mMap;
+    private ActivityTrackingBinding binding;
 
     private final static int REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         btnStart = findViewById(R.id.btnStart);
         btnStop = findViewById(R.id.btnStop);
@@ -90,6 +103,15 @@ public class TrackingActivity extends AppCompatActivity {
         }
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 }
