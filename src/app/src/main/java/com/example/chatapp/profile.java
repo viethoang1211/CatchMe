966220cf9c4c5c3db2b4834f4b8168224121ef34
vhdat <81861683+vhdat@users.noreply.github.com>
2023-01-10 23:07:24 +0700

    add profile UI

---
+++ src/app/src/main/java/com/example/chatapp/profile.java
@@ -0,0 +1,32 @@
+package com.example.chatapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+public class profile extends Fragment {
+
+    public static profile newInstance() {
+        return new profile();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((fragment_container)getActivity()).setup(toolbar);
+        TextView title  = view.findViewById(R.id.title);
+        title.setText("Profile");
+
+        return view;
+    }
+}
