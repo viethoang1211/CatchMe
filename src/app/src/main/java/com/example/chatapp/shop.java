cf5267575c4e5046b441a6c0ffa07826fee43769
vhdat <81861683+vhdat@users.noreply.github.com>
2023-01-10 21:31:08 +0700

    update shop choose

--- src/app/src/main/java/com/example/chatapp/shop.java
+++ src/app/src/main/java/com/example/chatapp/shop.java
@@ -1,6 +1,7 @@
 package com.example.chatapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +18,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.chatapp.Class.shopItem;
 import com.example.chatapp.adapters.shopAdapter;
 import com.example.chatapp.listeners.shopItemListener;
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 
 public class shop extends Fragment {
 
-    ArrayList<shopItem> shopItems = new ArrayList<>();
+    View view;
+    shopAdapter shopAdap;
+    MaterialButton physical,digital;
+
     private ShopViewModel mViewModel;
 
     public static shop newInstance() {
@@ -32,19 +37,44 @@ public class shop extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_shop, container, false);
+        view = inflater.inflate(R.layout.fragment_shop, container, false);
 
         Toolbar toolbar = view.findViewById(R.id.toolbar);
         ((fragment_container)getActivity()).setup(toolbar);
         TextView title  = view.findViewById(R.id.title);
         title.setText("Shop");
 
+        ArrayList<shopItem> shopItems2 = new ArrayList<>();
+        ArrayList<shopItem> shopItems = new ArrayList<>();
         shopItems.add(new shopItem("Viet Hoang",1231,"testing1",Boolean.TRUE));
         shopItems.add(new shopItem("Viet Hoang2",1232,"testing2",Boolean.TRUE));
-        shopItems.add(new shopItem("Viet Hoang3",1233,"testing3",Boolean.TRUE));
-        shopItems.add(new shopItem("Viet Hoang4",1234,"testing4",Boolean.FALSE));
-        RecyclerView RecyclerView= view.findViewById(R.id.recycler);
-        shopAdapter shopAdap = new shopAdapter(shopItems, new shopItemListener() {
+        shopItems2.add(new shopItem("Viet Hoang3",1233,"testing3",Boolean.TRUE));
+        shopItems2.add(new shopItem("Viet Hoang4",1234,"testing4",Boolean.FALSE));
+
+
+        physical = view.findViewById(R.id.physical);
+        digital = view.findViewById(R.id.digital);
+
+        choose(shopItems);
+
+        physical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choose(shopItems);
+            }
+        });
+
+        digital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choose(shopItems2);
+            }
+        });
+        return view;
+    }
+
+    private void choose(ArrayList l){
+        shopAdap =  new shopAdapter(l, new shopItemListener() {
             @Override
             public void onClickShopItem(shopItem item) {
                 Bundle bundle = new Bundle();
@@ -52,11 +82,10 @@ public class shop extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.action_global_shop_item_detail,bundle);
             }
         });
+        Log.d("tag","phy");
+        RecyclerView RecyclerView= view.findViewById(R.id.recycler);
         RecyclerView.setAdapter(shopAdap);
         RecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
-
-
-        return view;
     }
 
 //    @Override
